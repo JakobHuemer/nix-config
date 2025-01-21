e3e4c9efbd8bd63eb66b2d7f36223e2cec977496
Jakob Huemer-Fistelberger <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-01-21 08:55:25 +0100

    feat(starship): change continuation prompt

--- modules/home/shell/starship.nix
+++ modules/home/shell/starship.nix
@@ -6,7 +6,7 @@
     settings = {
       format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$gleam$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$quarto$raku$rlang$red$ruby$rust$scala$solidity$swift$terraform$typst$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$nats$direnv$env_var$crystal$custom$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$sudo$python$docker_context$character";
 
-      continuation_prompt = "❯ ";
+      continuation_prompt = "┆ ";
 
       character = {
         success_symbol = "[λ](bold purple)";
