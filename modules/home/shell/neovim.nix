a2e31d46ce1ce7aa985e44d19f1a1fad8dd26a2d
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-05-03 18:48:53 +0200

    feat(nixvim): put nixvim deps into nixvim module

--- modules/home/shell/neovim.nix
+++ modules/home/shell/neovim.nix
@@ -16,5 +16,20 @@
         catppuccin-nvim
       ];
     };
+
+    home.packages = with pkgs; [
+      treefmt2
+      shfmt
+      rust-analyzer
+      nixd
+      prettierd
+      yapf
+      nixfmt-rfc-style
+      rustfmt
+
+      vscode-langservers-extracted
+      vimPlugins.nvim-ts-autotag
+
+    ];
   };
 }
