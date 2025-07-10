e7ace0face80febb280e7c0550bb0f6f8fb4f066
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-07-10 23:26:58 +0200

    feat(ohmyposh): add git module

--- modules/home/shell/oh-my-posh.nix
+++ modules/home/shell/oh-my-posh.nix
@@ -24,6 +24,46 @@
                 max_depth = 3;
               };
             }
+            {
+              type = "git";
+              style = "plain";
+              foreground = "#10212E";
+              foreground_templates = [
+                "{{ if or (.Working.Changed) (.Staging.Changed) }}#E78C45{{ end }}"
+                "{{ if and (gt .Ahead 0) (gt .Behind 0) }}#F2D5CF{{ end }}"
+                "{{ if gt .Ahead 0 }}#8AADF4{{ end }}"
+                "{{ if gt .Behind 0 }}#F2D5CF{{ end }}"
+                "lightgreen"
+              ];
+              # foreground_templates = [
+              #   "{{ if or (.Working.Changed) (.Staging.Changed) }}#EA9B20{{ end }}"
+              #   "{{ if and (gt .Ahead 0) (gt .Behind 0) }}#E0B2A7{{ end }}"
+              #   "{{ if gt .Ahead 0 }}#AAC8EB{{ end }}"
+              #   "{{ if gt .Behind 0 }}#E0B2A7{{ end }}"
+              # ];
+              template =
+                "{{ .HEAD }}{{if .BranchStatus }} {{ .BranchStatus }}{{ end }}"
+                + "{{ if .Working.Changed }}  {{ .Working.String }}{{ end }}"
+                + "{{ if and (.Working.Changed) (.Staging.Changed) }} |{{ end }}"
+                + "{{ if .Staging.Changed }}  {{ .Staging.String }}{{ end }}"
+                + "{{ if gt .StashCount 0 }}  {{ .StashCount }}{{ end }} ";
+              properties = {
+                "fetch_stash_count" = true;
+                "fetch_status" = true;
+                "fetch_upstream_icon" = true;
+                "branch_icon" = "";
+                # looks nice but is unpractical but leaving it here for now
+                # "mapped_branches" = {
+                #   "dev*" = "🏗️/";
+                #   "feat/*" = "✨/";
+                #   "feature/*" = "✨/";
+                #   "release/*" = "📦/";
+                #   "hotfix/*" = "🔥/";
+                #   "bugfix/*" = "🐞/";
+                #   "test/*" = "🧪/";
+                # };
+              };
+            }
           ];
         }
         {
@@ -51,10 +91,23 @@
               type = "status";
               style = "plain";
               foreground = "red";
-              template =
-                "({{ if eq .Code 1 }}🫡{{ else if eq .Code 126 }}⛔"
-                + "{{ else if eq .Code 127 }}🔍{{ else if eq .Code 130 }}🧱"
-                + "{{ else if and (gt .Code 128) (lt .Code 160) }}⚡{{ else }}❓{{ end }}{{ .Code }}) ";
+              template = "({{ .String }}) ";
+
+              properties = {
+                status_template =
+                  # INFO: !!! here are literal escape unicodes !!!
+                  "{{if eq .Code 0}}[0m-[31;1m{{ else }}"
+                  + "{{ if eq .Code 1 }}🫡{{ else if eq .Code 126 }}⛔"
+                  + "{{ else if eq .Code 127 }}🔍{{ else if eq .Code 130 }}🧱"
+                  + "{{ else if and (gt .Code 128) (lt .Code 160) }}⚡{{ else }}❌"
+                  + "{{ end }}{{ .Code }}{{ end }}";
+              };
+            }
+            {
+              type = "nix-shell";
+              style = "plain";
+              foreground = "#84B0EB";
+              template = "{{ if ne .Type \"unknown\"}} {{ .Type }} {{ end }}";
             }
             {
               type = "text";
@@ -69,6 +122,16 @@
           ];
         }
       ];
+
+      transient_prompt = {
+        background = "transparent";
+        foreground = "#ffffff";
+        template = "λ ";
+        foreground_templates = [
+          "{{ if ne .Code 0 }}red{{ end }}"
+          "magenta"
+        ];
+      };
     };
   };
 }
