4c3579fadbe976ce5947780e36bbfdacbf8167b7
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-09-04 12:44:49 +0200

    feat(yt-music): add youtube-music options and plugins

--- modules/home/apps/youtube-music.nix
+++ modules/home/apps/youtube-music.nix
@@ -17,6 +17,54 @@
 
       options = {
         tray = true;
+
+        language = "en";
+
+        removeUpgradeButton = true;
+
+        resumeOnStart = false;
+
+        # TODO: Add a nice theme
+        themes = [];
+      };
+
+      plugins = {
+        adblocker = {
+          enabled = true;
+          blocker = "In player";
+        };
+
+        blur-nav-bar.enabled = true;
+
+        bypass-age-restrictions.enabled = true;
+
+        captions-selector.enabled = true;
+
+        crossfade = {
+          enabled = true;
+        };
+
+        exponential-volume.enabled = true;
+
+        in-app-menu.enabled = false;
+
+        performance-improvement.enabled = true;
+
+        precise-volume = {
+          enabled = true;
+          steps = 2;
+        };
+
+        synced-lyrics = {
+          enabled = true;
+        };
+
+        unobtrusive-player.enabled = true;
+
+        video-toggle = {
+          enabled = true;
+          align = "middle";
+        };
       };
     };
   };
