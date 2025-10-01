dd47b19fd909cf0ce961e25e771005cbe4476db7
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-10-02 00:40:14 +0200

    feat(limine): remove full bg

--- modules/nixos/misc/limine.nix
+++ modules/nixos/misc/limine.nix
@@ -20,8 +20,8 @@
 
       graphicalTerminal = {
         palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
-        background = "1e1e2e";
-        foreground = "cdd6f4";
+        # background = "1e1e2e";
+        # foreground = "cdd6f4";
       };
     };
   };
