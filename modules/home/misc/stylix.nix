d28ab553a8826d285b4617a597821dd78109545f
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-12-21 21:42:49 +0100

    feat(stylix): add nice nyancat bg images

--- modules/home/misc/stylix.nix
+++ modules/home/misc/stylix.nix
@@ -15,52 +15,62 @@
     useStylix = lib.mkEnableOption "enables stylix";
   };
 
-  config = lib.mkIf config.useStylix {
-    stylix = {
-      enable = true;
+  config = let
+    bgImages = {
+      nyancat-space-kid-drawn = ../../../assets/img/bg/nyancat-space-kid-drawn.jpg;
+      nyancat-space-drawn = ../../../assets/img/bg/nyancat-space-drawn.jpg;
+      windows-10-lock-mc = ../../../assets/img/bg/windows_10_lock_mc.jpg;
+      planet = ../../../assets/img/bg/planet.jpg;
+      creeper-fancy = ../../../assets/img/bg/creeper-fancy.jpg;
+      creeper = ../../../assets/img/bg/creeper.jpg;
+    };
+  in
+    lib.mkIf config.useStylix {
+      stylix = {
+        enable = true;
 
-      enableReleaseChecks = false;
+        enableReleaseChecks = false;
 
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-      polarity = "dark";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+        polarity = "dark";
 
-      image = ../../../assets/img/bg/windows_10_lock_mc.jpg;
+        image = bgImages.nyancat-space-drawn;
 
-      targets.nixcord.enable = false;
+        targets.nixcord.enable = false;
 
-      targets.zen-browser.enable = false;
+        targets.zen-browser.enable = false;
 
-      fonts = {
-        serif = {
-          package = pkgs.dejavu_fonts;
-          name = "DejaVu Serif";
+        fonts = {
+          serif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Serif";
+          };
+
+          sansSerif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Sans";
+          };
+
+          monospace = {
+            package = pkgs.nerd-fonts.jetbrains-mono;
+            name = "JetBrains Mono";
+          };
+
+          # emoji = {
+          #   package = pkgs.noto-fonts-color-emoji;
+          #   name = "Noto Color Emoji";
+          # };
+          emoji = {
+            package = inputs.apple-emoji-linux.packages.${system}.apple-emoji-linux;
+            name = "Apple Color Emoji";
+          };
         };
 
-        sansSerif = {
-          package = pkgs.dejavu_fonts;
-          name = "DejaVu Sans";
+        cursor = {
+          name = "Banana";
+          package = pkgs.banana-cursor;
+          size = 24;
         };
-
-        monospace = {
-          package = pkgs.nerd-fonts.jetbrains-mono;
-          name = "JetBrains Mono";
-        };
-
-        # emoji = {
-        #   package = pkgs.noto-fonts-color-emoji;
-        #   name = "Noto Color Emoji";
-        # };
-        emoji = {
-          package = inputs.apple-emoji-linux.packages.${system}.apple-emoji-linux;
-          name = "Apple Color Emoji";
-        };
-      };
-
-      cursor = {
-        name = "Banana";
-        package = pkgs.banana-cursor;
-        size = 24;
       };
     };
-  };
 }
