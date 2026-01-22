6890adf30ec4ecbd057a2ba301a43c43fc8a997f
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-01-22 19:42:43 +0100

    feat(syncthing): add sta01 host

--- modules/home/apps/syncthing.nix
+++ modules/home/apps/syncthing.nix
@@ -25,6 +25,7 @@
         devices = {
           "nixbook".id = "G57PZ76-IT5IMMC-C3KQEN3-EH5X2JE-SKAUU7M-C7CHEWG-U2IVIDO-A3CUXAG";
           "pi4".id = "ZFDPFSJ-VLG6YDV-DH76C72-4NG7WDX-RFRO3Y4-6NTRGTP-NAEDUKW-IAOFEAS";
+          "sta01".id = "BV2BYWN-KFLGCYV-THMUZ34-DNYOSJ6-6MQLCQ3-N6LR4HH-2QJDI5S-LR6KJAB";
         };
 
         folders = {
@@ -34,6 +35,7 @@
             devices = [
               "nixbook"
               "pi4"
+              "sta01"
             ];
           };
         };
