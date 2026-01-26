94173bdbddf38db379691ae69e402c873fe2a0a4
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-01-26 19:55:10 +0100

    feat(syncthing): add mbp2p id

--- modules/home/apps/syncthing.nix
+++ modules/home/apps/syncthing.nix
@@ -22,6 +22,7 @@
           "nixbook".id = "G57PZ76-IT5IMMC-C3KQEN3-EH5X2JE-SKAUU7M-C7CHEWG-U2IVIDO-A3CUXAG";
           "pi4".id = "ZFDPFSJ-VLG6YDV-DH76C72-4NG7WDX-RFRO3Y4-6NTRGTP-NAEDUKW-IAOFEAS";
           "sta01".id = "BV2BYWN-KFLGCYV-THMUZ34-DNYOSJ6-6MQLCQ3-N6LR4HH-2QJDI5S-LR6KJAB";
+          "mbp2p".id = "VSEHSRN-GLJKODA-YSHOY3C-Z42LV32-M4UHIRV-64XHYJQ-EUE3FHV-F5INSAH";
         };
 
         folders = {
@@ -32,6 +33,7 @@
               "nixbook"
               "pi4"
               "sta01"
+              "mbp2p"
             ];
           };
         };
