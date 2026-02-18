ff0b85e459ad985dd399948d84f21adb3ea5a7c2
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-02-18 22:59:57 +0100

    feat(opencode): add local ollama provider configuration

--- modules/home/apps/opencode.nix
+++ modules/home/apps/opencode.nix
@@ -12,6 +12,21 @@
 
       settings = {
         autoupdate = true;
+
+        provider = {
+          ollama = {
+            name = "Ollama (local)";
+            npm = "@ai-sdk/openai-compatible";
+            options = {
+              # baseURL = "http://sta01.h.fistel.dev:11434/v1";
+              baseURL = "http://127.0.0.1:11434/v1";
+            };
+
+            models = {
+              "qwen3-coder:30b".name = "qwen3-coder:30b";
+            };
+          };
+        };
       };
     };
   };
