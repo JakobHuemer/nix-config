1ba5b2d7c96f9b472dc9467d615333ea880a2296
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-02-20 04:01:22 +0100

    feat(ollama): increase context length
    experiment and research about per model context length
    and how context length works depending on the system

--- modules/nixos/misc/ollama.nix
+++ modules/nixos/misc/ollama.nix
@@ -21,7 +21,7 @@
       environmentVariables = {
         OLLAMA_FLASH_ATTENTION = "0";
         HIP_VISIBLE_DEVICES = "0";
-        OLLAMA_CONTEXT_LENGTH = "16384";
+        OLLAMA_CONTEXT_LENGTH = "65536";
       };
     };
 
