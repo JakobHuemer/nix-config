5b28ea2f8d56beae00400abd5909242d21f3031d
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-03-15 23:53:34 +0100

    feat(opencode): disable old local models because they're unused

--- modules/home/apps/opencode.nix
+++ modules/home/apps/opencode.nix
@@ -13,39 +13,39 @@
       settings = {
         autoupdate = true;
 
-        provider = {
-          ollama = {
-            name = "Ollama (sta01)";
-            npm = "@ai-sdk/openai-compatible";
-            options = {
-              baseURL = "http://sta01.h.fistel.dev:11434/v1";
-            };
-
-            models = {
-              "qwen3-coder:30b" = {
-                name = "qwen3-coder:30b";
-                limit = {
-                  context = 256000;
-                  output = 256000;
-                };
-              };
-              "gpt-oss:20b" = {
-                name = "gpt-oss:20b";
-                limit = {
-                  context = 128000;
-                  output = 128000;
-                };
-              };
-              "glm-4.7-flash:latest" = {
-                name = "glm-4.7-flash:latest";
-                limit = {
-                  context = 189000;
-                  output = 198000;
-                };
-              };
-            };
-          };
-        };
+        # provider = {
+        #   ollama = {
+        #     name = "Ollama (sta01)";
+        #     npm = "@ai-sdk/openai-compatible";
+        #     options = {
+        #       baseURL = "http://sta01.h.fistel.dev:11434/v1";
+        #     };
+        #
+        #     models = {
+        #       "qwen3-coder:30b" = {
+        #         name = "qwen3-coder:30b";
+        #         limit = {
+        #           context = 256000;
+        #           output = 256000;
+        #         };
+        #       };
+        #       "gpt-oss:20b" = {
+        #         name = "gpt-oss:20b";
+        #         limit = {
+        #           context = 128000;
+        #           output = 128000;
+        #         };
+        #       };
+        #       "glm-4.7-flash:latest" = {
+        #         name = "glm-4.7-flash:latest";
+        #         limit = {
+        #           context = 189000;
+        #           output = 198000;
+        #         };
+        #       };
+        #     };
+        #   };
+        # };
       };
     };
   };
