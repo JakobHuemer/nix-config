1a2fb213601bc1ba81a634b4026969385cbb9bfd
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-04-09 19:58:37 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
     extra-substituters = [
       "https://nixos-apple-silicon.cachix.org"
       "https://hyprland.cachix.org"
-      "https://cache.garnix.io"
+      # "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
