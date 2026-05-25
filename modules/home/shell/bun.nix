21cb616871d7d8487e730b026487292331f991a0
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-05-25 13:24:40 +0200

    revert(bun): do not set security.scanner and frozenLockfile

--- modules/home/shell/bun.nix
+++ modules/home/shell/bun.nix
@@ -10,10 +10,10 @@
           minimumReleaseAge = 1209600; # 2 weeks
           minimumReleaseAgeExcludes = ["@types/node" "typescript"];
 
-          frozenLockfile = true;
+          # frozenLockfile = true;
           exact = true;
 
-          security.scanner = "bun-osv-scanner";
+          # security.scanner = "bun-osv-scanner";
 
           linker = "isolated";
         };
