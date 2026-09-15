4760340940b000898aaa92cfbb7cecc027c8bebf
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-09-15 10:13:33 +0200

    feat(nixbook): add 80% charge threshold at which point it will switch to AC

--- hosts/nixbook/configuration.nix
+++ hosts/nixbook/configuration.nix
@@ -25,6 +25,12 @@
   #   "brcmfmac.feature_disable=0x82000"
   # ];
 
+  # makes charging stop at 80 percent and will switch to AC.
+  # Automatically, if battery reaches 75 it will charge again.
+  systemd.tmpfiles.rules = [
+    "w /sys/class/power_supply/macsmc-battery/charge_control_end_threshold - - - - 80"
+  ];
+
   # networking.networkmanager.wifi.scanRandMacAddress = false;
   # networking.networkmanager.wifi.macAddress = "preserve";
 
