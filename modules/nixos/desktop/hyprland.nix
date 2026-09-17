5e540df6e1c81cec1ab7d3bb1dc4dcca7b969b56
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-09-17 22:27:01 +0200

    feat(hyprland): add hyprpolkitagent for authentication stuff

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -56,6 +56,10 @@
         playerctl
       ];
 
+      services.hyprpolkitagent = {
+        enable = true;
+      };
+
       services.wpaperd = {
         enable = true;
         package = pkgs.custom.wpaperd;
